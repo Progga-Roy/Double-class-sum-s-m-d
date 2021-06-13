22ae5aff0f84b7de6cb9f1b10b631529dc031a97
Progga-Roy <76958372+Progga-Roy@users.noreply.github.com>
2021-06-13 20:16:54 +0600

    Add files via upload
    half

---
+++ math.java
@@ -0,0 +1,21 @@
+public class math {
+     void addition (int a, int b) {
+        int add = a + b;
+        System.out.printf("\nSummation = %d" ,add);
+
+    }
+      void subtraction (int a, int b) {
+        int s = a - b;
+        System.out.printf("\nSubtraction = %d",s);
+    }
+    void multiplication (int a, int b) {
+        int m = a * b;
+        System.out.printf("\nMultiplication = %d",m);
+
+    }
+    void division (int a, int b) {
+        int d = a / b;
+        System.out.printf("\nDivision = %d",d);
+
+    }
+}
